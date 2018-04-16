1ee1547687d5d1eff2a382aa5ad8882044382035
Ahmed Albeiruti <ahmed.albieruti@gmail.com>
2018-04-16 21:47:27 +1000

    configuring various dev helper tools

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const MyTitle = function(props) {
-  return React.createElement('div', null, React.createElement('h1', { style: { color: props.color } }, props.title));
+  return React.createElement(
+    'div',
+    null,
+    React.createElement('h1', { style: { color: props.color } }, props.title)
+  );
 };
 const MyFirstComponent = function() {
   return React.createElement('div', null, [
-    React.createElement(MyTitle, { title: 'Game of thrones', color: 'YellowGreen' }),
-    React.createElement(MyTitle, { title: 'Stranger things', color: 'GreenYellow' }),
+    React.createElement(MyTitle, {
+      title: 'Game of thrones',
+      color: 'YellowGreen'
+    }),
+    React.createElement(MyTitle, {
+      title: 'Stranger things',
+      color: 'GreenYellow'
+    }),
     React.createElement(MyTitle, { title: 'Rick and Morty', color: 'Lime' }),
-    React.createElement(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+    React.createElement(MyTitle, { title: 'House of cards', color: 'peru' })
   ]);
 };
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'));
+render(React.createElement(MyFirstComponent), document.getElementById('app'));
