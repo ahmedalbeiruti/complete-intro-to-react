1a29a9f2e10aadd356d66d55c800b07b99c0312c
Ahmed Albeiruti <ahmed.albieruti@gmail.com>
2018-04-18 00:55:50 +1000

    starting search video app. added react route used hashrouter & browserrouter

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './landing';
+import Search from './Search';
 
-const MyTitle = function(props) {
-  // return React.createElement(
-  //   'div',
-  //   null,
-  //   React.createElement('h1', { style: { color: props.color } }, props.title)
-  // );
-  return (
-    <div>
-      <h1 style={{ color: props.color }}>{props.title}</h1>
-    </div>
-  );
-};
-const MyFirstComponent = function() {
-  return (
-    <div id="my-first-component">
-      <MyTitle title="Game of thrones" color="YellowGreen" />
-      <MyTitle title="Stranger things" color="GreenYellow" />
-      <MyTitle title="Rick and Morty" color="burlywoodßwe" />
-      <MyTitle title="House of cards" color="peru" />
-    </div>
-  );
-};
+const FourOhFour = () => <h1>404</h1>;
 
-render(<MyFirstComponent />, document.getElementById('app'));
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+render(<App />, document.getElementById('app'));
